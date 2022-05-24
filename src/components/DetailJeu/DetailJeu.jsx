43c6523749df67fa73749ba67b33e6c9d6686aec
Laconiq <laconiqofficial@gmail.com>
2022-05-24 20:44:14 +0200

    Div centrée + Responsive
    Centrage de la div verticalement et c'est responsive !

--- src/components/DetailJeu/DetailJeu.jsx
+++ src/components/DetailJeu/DetailJeu.jsx
@@ -6,14 +6,14 @@ const Detail_jeu = () => {
     <>
     <body>
       <div className='detailjeu_container'>
-        <div className='text'>
-          <div className='text__container'>
+        <div className='text_container'>
+          <div className='text'>
             <h1>Nom du jeu</h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit doloribus earum neque et facilis, consectetur amet, iusto culpa qui provident eligendi at? Nemo, facilis. Veniam facere labore voluptatem quaerat harum.</p>
           </div>
         </div>       
-        <div className='detailjeu_image'>
-          <img src="https://lanouvelle-lejeu.fr/wp-content/uploads/2022/04/pic.png" alt="image-lanouvelle" />
+        <div className='detailjeu_image_container'>
+          <img className='detailjeu_image' src="https://browsecat.net/sites/default/files/windows-1920x1080-wallpapers-125693-656467-6080285.png" alt="image-lanouvelle" />
         </div>
       </div>
     </body>
