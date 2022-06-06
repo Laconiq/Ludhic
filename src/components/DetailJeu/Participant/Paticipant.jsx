e093d72699aac96b745acc0ad68636d340317903
NymePse <arnaud.bijou@gmail.com>
2022-06-06 18:41:35 +0200

    Tentative changement Participants (échec, array vide) A REVOIR

--- src/components/DetailJeu/Participant/Paticipant.jsx
+++ src/components/DetailJeu/Participant/Paticipant.jsx
@@ -1,45 +1,69 @@
-import React from 'react'
-import '../../DetailJeu/detailjeu.css'
+import React from 'react';
+import '../../DetailJeu/detailjeu.css';
+import { getDatabase, ref, get, child } from 'firebase/database';
 
-const data = [
-    {
-        id: 1,
-        prenom: 'Bastien',
-        nom: 'Okonski',
-        role: 'Level Designer'
-    },
 
-    {
-        id: 2,
-        prenom: 'Arnaud',
-        nom: 'Lysensoone-Bijou',
-        role: 'Narrative Designer'
-    },
+class Participant extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            membres: [{id:0,nom:"Test",prenom:"Osterone",poste:"Rien"}]
+        };
+    }
 
-    {
-        id: 3,
-        prenom: 'Brice',
-        nom: 'Graulier',
-        role: 'Développeur'
-    },
-]
+    componentDidMount() {
+        let listeMembres = new Array();
 
-function Participant() {
-    return (
-        <section id='etudiant'>
-        <h2>Étudiant participants au projet :</h2>
-            {
-                data.map(({id, prenom, nom, role}) => {
-                    return (
-                        <article key={id}>
-                            <p className='etudiant'>{prenom} {nom} : {role}</p>
-                        </article>
+        const jeuId = 1;
+        const dbRef = ref(getDatabase());
+        get(child(dbRef, `Jeu/${jeuId}/Membre`)).then((snapshot) => {
+        if (snapshot.exists()) 
+        {
+            snapshot.val().forEach(element => {
+                get(child(dbRef, `Compte/${element.Compte}`)).then((snapshot) => {
+                    if (snapshot.exists())
+                    {
+                        let membre = {
+                            id:element.Compte,
+                            nom:snapshot.val().Nom,
+                            prenom:snapshot.val().Prenom,
+                            poste:element.Poste
+                        }
+                        
+                        listeMembres.push(membre);
+                    }
+                });
+            });
+            console.log(listeMembres);
+            console.log(this.state.membres);
+            this.setState({membres: listeMembres});
+        } 
+        else 
+        {
+            console.log("No data available");
+        }
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+
+    render() {
+        return (
+            <section id='etudiant'>
+            <h2>Étudiant participants au projet :</h2>
+                {
+                    this.state.membres.map(({id, nom, prenom, poste}) => {
+                        return (
+                            <article key={id}>
+                                <p className='etudiant'>{prenom} {nom} : {poste}</p>
+                            </article>
+                        )
+                    }
                     )
                 }
-                )
-            }
-        </section>
-    )
+            </section>
+        )
+    }
 }
 
 export default Participant
