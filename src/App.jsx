3f42429c59896b25e1672f7a57168cb75e541b63
Laconiq <laconiqofficial@gmail.com>
2022-06-07 21:19:44 +0200

    Affichage grille page "Jeux"

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import './App.css';
-import Nav from './components/nav/Nav';
+import Nav from './components/Nav/Nav';
 import DetailJeu from './components/DetailJeu/DetailJeu';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Accueil from './components/Accueil/Accueil';
+import Jeux from './components/Jeux/Jeux';
 import {Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/jeux' element={<><Nav/><DetailJeu/><Footer/></>}/>
+        <Route path='/jeux/nomdujeu' element={<><Nav/><DetailJeu/><Footer/></>}/>
         <Route path='/' element={<><Nav/><Accueil/><Footer/></>}/>
+        <Route path='/jeux' element={<><Nav/><Jeux/><Footer/></>}/>
       </Routes>
     </>
   );
---
+++ src/components/Jeux/Jeux.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import './jeux.css'
+
+const data = [
+    {
+        id : 1,
+        image: 'https://lanouvelle-lejeu.fr/wp-content/uploads/2022/04/pic.png',
+        titre: 'La Nouvelle',
+        description: 'Blablabla',
+        lien:'',
+    },
+
+    {
+        id : 2,
+        image: 'https://lanouvelle-lejeu.fr/wp-content/uploads/2022/04/pic.png',
+        titre: 'Zemouurigan',
+        description: 'Blablabla',
+        lien:'',
+    },
+
+    {
+        id : 3,
+        image: 'https://lanouvelle-lejeu.fr/wp-content/uploads/2022/04/pic.png',
+        titre: 'Dernière Sueur',
+        description: 'Blablabla',
+        lien:'',
+    },
+
+    {
+        id : 4,
+        image: 'https://lanouvelle-lejeu.fr/wp-content/uploads/2022/04/pic.png',
+        titre: "C'est far",
+        description: 'Blablabla',
+        lien:'',
+    }
+]
+
+function Jeux() {
+  return (
+    <div className='jeux-container'>
+        {
+          data.map(({id, image, titre, description}) => {
+            return (
+              <article key={id} className='jeux-item'>
+                <div>
+                  <img src={image} alt={titre} className='jeux-image'/>
+                </div>
+                <h3 className='jeux-titre'>{titre}</h3>
+                <small className='jeux-description'>{description}</small>
+              </article>
+            )
+          })
+        }
+    </div>
+  )
+}
+
+export default Jeux
