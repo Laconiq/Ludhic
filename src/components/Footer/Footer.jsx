6bff52856cc0569dbe207d96ac1a6a4bce9b0d1b
Laconiq <laconiqofficial@gmail.com>
2022-06-13 11:59:52 +0200

    Lien "faire un don"
    À nous la richesse !

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ class Footer extends React.Component {
     <div className='footer-container'>
         <div className='ligne ligne-top'></div>
         <p className='footer-text'>© 2022 Ludhic. Tous droits réservés.</p>
-        <a href="#donne l'argent" className='bouton-footer'>Faire un don</a>
+        <a href="https://www.paypal.com/donate/?hosted_button_id=ARNL35P5H8PKS" target="_blank" className='bouton-footer'>Faire un don</a>
         <div className='ligne'></div>
     </div>
     <div className='footer-bottom'>
