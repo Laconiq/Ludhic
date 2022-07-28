af7333ca9c1e9d7eea6f28ee86ca6d6aade803a5
Laconiq <laconiqofficial@gmail.com>
2022-07-28 13:54:10 +0200

    CSS page "Administration"

--- src/components/Administration/Administration.jsx
+++ src/components/Administration/Administration.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 function Administration() {
     return (
         <>
-            <ul>
-                <li><a href='./jeu'>Jeu</a></li>
-                <li><a href='./rendu'>Rendu</a></li>
-                <li><a href='./materiel-cl'>Creative Lab</a></li>
-                <li><a href='./inscription'>Inscription</a></li>
+            <ul className='admin-container'>
+                <li><div className='bouton'><a href='./jeu'>Jeu</a></div></li>
+                <li><div className='bouton'><a href='./rendu'>Rendu</a></div></li>
+                <li><div className='bouton'><a href='./materiel-cl'>Creative Lab</a></div></li>
+                <li><div className='bouton'><a href='./inscription'>Inscription</a></div></li>
             </ul>
         </>
     )
