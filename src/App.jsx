24b5717f18ec59919008ac22274637789b71d0fe
Arnaud Lysensoone Bijou <arnaud.bijou@gmail.com>
2022-08-01 13:38:56 +0200

    Coeur vérification connexion

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import { getAuth, onAuthStateChanged  } from 'firebase/auth';
+import { child, get, getDatabase, ref, push, update } from "firebase/database";
 import NavBar from './components/NavBar/NavBar';
 import PageJeu from './components/Jeu/PageJeu';
 import Header from './components/Header/Header';
@@ -16,6 +19,54 @@ import FormulaireConnexion from './components/Connexion/FormulaireConnexion';
 import CreativeLab from './components/CreativeLab/CreativeLab';
 
 function App() {
+  const [compte, setCompte] = useState(false);
+
+  useEffect(() => {
+    onAuthStateChanged(getAuth(), (user) => {
+      if(user)
+      {
+        get(child(ref(getDatabase()),`Compte/${user.uid}`))
+        .then((snapshot) => {
+          const tmp = snapshot.val();
+          setCompte({
+            id: user.uid,
+            admin: tmp.Admin,
+            nom: tmp.Nom,
+            prenom: tmp.Prenom,
+            formation: tmp.Formation,
+            niveau: tmp.Niveau
+          });
+        })
+        .catch(
+          //TODO gérer si erreur de requête
+        );
+        //TODO gérer si aucune info dans BDD
+      }
+      else
+      {
+        setCompte({
+          id: false,
+          admin: false,
+          nom: false,
+          prenom: false,
+          formation: false,
+          niveau: false
+        });
+      }
+    });
+  },[])
+
+  useEffect(() => {
+    if(compte) console.log("Compte :", compte);
+  },[compte])
+
+  //TODO Vérification connexion de l'utilisateur
+  /*
+  * Utilisateur conservé dans State ici
+  * Passe l'utilisateur en props dans les components qui en auraient besoin
+  * (exemple : Navbar, pages qui ont besoin d'être connecté, pages réservées aux admins...)
+  */
+
   return (
     <>
       <NavBar/>
@@ -23,18 +74,18 @@ function App() {
         <Route path='/jeux/:id' element={<PageJeu/>}/>
         <Route path='/' element={<PageAccueil/>}/>
         <Route path='/jeux' element={<PageListeJeux/>}/>
-        <Route path='/calendrier' element={<Calendrier/>}/>
+        <Route path='/calendrier' element={<Calendrier utilisateur={compte}/>}/>
+        <Route path='/inscription' element={<FormulaireInscription/>}/>
         <Route path='/connexion' element={<FormulaireConnexion/>}/>
-        <Route path='/creative-lab' element={<CreativeLab/>}/>
+        <Route path='/creative-lab' element={<CreativeLab utilisateur={compte}/>}/>
         {/* CHEMINS POUR ADMINISTRATION */}
-        <Route path='/administration' element={<Administration/>}/>
-        <Route path='/administration/inscription' element={<FormulaireInscription/>}/>
-        <Route path='/administration/jeu' element={<FormulaireJeu/>}/>
-        <Route path='/administration/jeu/:id' element={<FormulaireJeu/>}/>
-        <Route path='/administration/rendu' element={<FormulaireRendu/>}/>
-        <Route path='/administration/rendu/:id' element={<FormulaireRendu/>}/>
-        <Route path='/administration/materiel-cl' element={<FormulaireCreativeLab/>}/>
-        <Route path='/administration/materiel-cl/:id' element={<FormulaireCreativeLab/>}/>
+        <Route path='/administration' element={<Administration utilisateur={compte}/>}/>
+        <Route path='/administration/jeu' element={<FormulaireJeu utilisateur={compte}/>}/>
+        <Route path='/administration/jeu/:id' element={<FormulaireJeu utilisateur={compte}/>}/>
+        <Route path='/administration/rendu' element={<FormulaireRendu utilisateur={compte}/>}/>
+        <Route path='/administration/rendu/:id' element={<FormulaireRendu utilisateur={compte}/>}/>
+        <Route path='/administration/materiel-cl' element={<FormulaireCreativeLab utilisateur={compte}/>}/>
+        <Route path='/administration/materiel-cl/:id' element={<FormulaireCreativeLab utilisateur={compte}/>}/>
       </Routes>
       <Footer/>
     </>
--- src/helpers/compte.js
+++ src/helpers/compte.js
@@ -1,24 +1,14 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
-export function estConnecte(changerPage = false, redirection = "/") {
-    onAuthStateChanged(getAuth(), (user) => {
-        if(user)
-        {
-            //TODO vérifier que le compte a des données dans la BDD
-            return true;
-        }
+export function estConnecte(utilisateur, changerPage = false, navigate, redirection = "/") {
+    if(typeof utilisateur === "object")
+    {
+        if(utilisateur.id) return true;
         else
         {
-            const navigate = useNavigate();
             if(changerPage) navigate(redirection);
-            else return false;
+            return false;
         }
-    });
-}
-
-export function estAdmin(changerPage = false, redirection = "/") {
-    //TODO récupérer données du compte sur la BDD
-    //Retourner valeur de Compte/Admin (true ou false)
-    //Si false et changerpage, redirect à redirection
+    }
 }
