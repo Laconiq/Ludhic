ba2b608790722c7fa722fa68fa85002bff8e653e
Arnaud Lysensoone Bijou <arnaud.bijou@gmail.com>
2022-08-04 13:26:08 +0200

    PAF on cache ce qu'on veux pas dans l'alpha

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { getAuth, onAuthStateChanged  } from 'firebase/auth';
 import { child, get, getDatabase, ref, push, update } from "firebase/database";
 import NavBar from './components/NavBar/NavBar';
 import PageJeu from './components/Jeu/PageJeu';
-import Header from './components/Header/Header';
+//import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import PageAccueil from './components/Accueil/PageAccueil';
 import PageListeJeux from './components/ListeJeux/PageListeJeux';
@@ -12,11 +12,11 @@ import {Routes, Route} from "react-router-dom"
 import Administration from './components/Administration/Administration';
 import FormulaireJeu from './components/Administration/FormulaireJeu'
 import FormulaireInscription from './components/Administration/FormulaireInscription'
-import FormulaireRendu from './components/Administration/FormulaireRendu';
-import FormulaireCreativeLab from './components/Administration/FormulaireCreativeLab';
-import Calendrier from './components/Calendrier/Calendrier';
+//import FormulaireRendu from './components/Administration/FormulaireRendu';
+//import FormulaireCreativeLab from './components/Administration/FormulaireCreativeLab';
+//import Calendrier from './components/Calendrier/Calendrier';
 import FormulaireConnexion from './components/Connexion/FormulaireConnexion';
-import CreativeLab from './components/CreativeLab/CreativeLab';
+//import CreativeLab from './components/CreativeLab/CreativeLab';
 
 function App() {
   const [compte, setCompte] = useState(false);
@@ -74,18 +74,24 @@ function App() {
         <Route path='/jeux/:id' element={<PageJeu/>}/>
         <Route path='/' element={<PageAccueil/>}/>
         <Route path='/jeux' element={<PageListeJeux/>}/>
-        <Route path='/calendrier' element={<Calendrier utilisateur={compte}/>}/>
         <Route path='/inscription' element={<FormulaireInscription/>}/>
         <Route path='/connexion' element={<FormulaireConnexion/>}/>
-        <Route path='/creative-lab' element={<CreativeLab utilisateur={compte}/>}/>
+        
         {/* CHEMINS POUR ADMINISTRATION */}
         <Route path='/administration' element={<Administration utilisateur={compte}/>}/>
         <Route path='/administration/jeu' element={<FormulaireJeu utilisateur={compte}/>}/>
         <Route path='/administration/jeu/:id' element={<FormulaireJeu utilisateur={compte}/>}/>
-        <Route path='/administration/rendu' element={<FormulaireRendu utilisateur={compte}/>}/>
-        <Route path='/administration/rendu/:id' element={<FormulaireRendu utilisateur={compte}/>}/>
-        <Route path='/administration/materiel-cl' element={<FormulaireCreativeLab utilisateur={compte}/>}/>
-        <Route path='/administration/materiel-cl/:id' element={<FormulaireCreativeLab utilisateur={compte}/>}/>
+        
+        {
+          /*
+            <Route path='/calendrier' element={<Calendrier utilisateur={compte}/>}/>
+            <Route path='/creative-lab' element={<CreativeLab utilisateur={compte}/>}/>
+            <Route path='/administration/rendu' element={<FormulaireRendu utilisateur={compte}/>}/>
+            <Route path='/administration/rendu/:id' element={<FormulaireRendu utilisateur={compte}/>}/>
+            <Route path='/administration/materiel-cl' element={<FormulaireCreativeLab utilisateur={compte}/>}/>
+            <Route path='/administration/materiel-cl/:id' element={<FormulaireCreativeLab utilisateur={compte}/>}/>
+          */
+        }
       </Routes>
       <Footer/>
     </>
