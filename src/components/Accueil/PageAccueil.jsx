52339897c091a1a7ed2aac17d28b5a6e4985effd
Bastien <laconiqofficial@gmail.com>
2022-08-04 09:42:28 +0200

    Modification "Quoi de neuf ?"

--- src/components/Accueil/PageAccueil.jsx
+++ src/components/Accueil/PageAccueil.jsx
@@ -33,8 +33,10 @@ export default class PageAccueil extends Component {
 
       <div className='home-background'>
         <div className='home-line'></div>
-        <h2>Quoi de neuf</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam quo doloribus, expedita veniam corporis omnis pariatur beatae dicta deserunt possimus veritatis autem corrupti aut rem officiis laborum rerum voluptatum maiores?</p>
+        <h2>Quoi de neuf ?</h2>
+        <p>Ludhic rentre officiellement dans sa phase de bêta fermé !
+          <br/>Pour toutes questions ou informations supplémentaires contactez nous sur <a href="mailto:ludhic.association@gmail.com">ludhic.association@gmail.com</a> ou sur Discord à <a href="https://discord.ludhic.fr">discord.ludhic.fr</a>
+        </p>
       </div>
 
     {/* BOUTON ADMIN */}
