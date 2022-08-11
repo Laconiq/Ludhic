dccf77ee789db56f5101934ca8fe53a82e0440ab
Arnaud Lysensoone Bijou <arnaud.bijou@gmail.com>
2022-08-11 16:58:36 +0200

    Documentation des fonctions

--- src/helpers/fonctionsFormulaires.js
+++ src/helpers/fonctionsFormulaires.js
@@ -1,4 +1,9 @@
-//Fonctions de gestion des données du formulaire
+/**
+*   La fonction permet de mettre à jour les informations de State lié à un formulaire à chaque modification de données.
+*   @param  {Event} event L'évènement permettant de récupérer les données du champ modifié
+*   @param  {Array} stateVar Le State Array dans son état actuel.
+*   @param  {Function} stateSetterFunction La fonction de mise à jour du State Array
+*/
 export function modificationFormulaire(event, stateVar, stateSetterFunction) {
     const target = event.target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
--- src/helpers/images.js
+++ src/helpers/images.js
@@ -1,12 +1,21 @@
 import FileResizer from 'react-image-file-resizer';
 
-export function compresserImage(image, format = "PNG", qualite = 100) 
+/**
+ * Cette fonction permet de compresser une image.
+ * @param {File}    image   L'image à compresser.
+ * @param {Number}  largeurMax  Largeur maximum après compression.
+ * @param {Number}  hauteurMax  Hauteur maximum après compression.
+ * @param {String}  format  Le format d'image qui résulterat. Peut être "PNG" ou "JPEG".
+ * @param {Number}  qualite Le taux de qualité finale appliqué aux JPEG. Entre 0 (plus basse) et 100 (plus haute).
+ * @returns {File} Le fichier image modifié.
+ */
+export function compresserImage(image, largeurMax, hauteurMax, format = "PNG", qualite = 100) 
 {
     return new Promise((resolve) => {
         FileResizer.imageFileResizer(
             image,
-            1920,
-            1080,
+            largeurMax,
+            hauteurMax,
             format,
             qualite,
             0,
@@ -17,4 +26,6 @@ export function compresserImage(image, format = "PNG", qualite = 100)
         )
     });
 }
+
+//TODO Rajouter paramètres maxWidth et maxHeight
         
