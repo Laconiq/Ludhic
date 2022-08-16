4b5936d71574efed7064d85ac37db9902d13e2a7
Laconiq <laconiqofficial@gmail.com>
2022-08-16 19:02:31 +0200

    Administration : Prototype

--- src/components/Administration/Administration.jsx
+++ src/components/Administration/Administration.jsx
@@ -2,6 +2,33 @@ import React from 'react'
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { estConnecte } from '../../helpers/compte';
+import './administration.css';
+
+const data = [
+    {
+        id: 1,
+        image: 'https://cdn.discordapp.com/attachments/973168025819824169/1009141920854978630/Kakemono_qui_pense.png',
+        title: 'Jeux',
+        subtitle: 'soustitre',
+        lien: '/administration/jeu',
+    },
+
+    {
+        id: 2,
+        image: 'https://cdn.discordapp.com/attachments/973168025819824169/1009141921400225872/Monsieur_qui_pense.png',
+        title: 'Rendu',
+        subtitle: 'soustitre',
+        lien: '/rendu',
+    },
+
+    {
+        id: 3,
+        image: 'https://cdn.discordapp.com/attachments/973168025819824169/1009141920502644896/Creative_lab_qui_pense.png',
+        title: 'Creative Lab',
+        subtitle: 'soustitre',
+        lien: '/cl',
+    }
+]
 
 function Administration(props) {
     const [compte, setCompte] = useState(false);
@@ -24,12 +51,27 @@ function Administration(props) {
             { document.title = "Administration - Ludhic" }
             <h1 className='form-title'>Administration</h1>
             <div className='form-line'></div>
-            <ul className='admin-container'>
-                <li><a href='./jeu'>Jeu</a></li>
-                <li><a href='./rendu'>Rendu</a></li>
-                <li><a href='./materiel-cl'>Creative Lab</a></li>
-                <li><a href='../inscription'>Inscription</a></li>
-            </ul>
+
+            <div>
+                {
+                    data.map(({id, image, title, subtitle, lien}) => {
+                        return (
+                            <a href={lien}>
+                                <article className='administration-container' key={id}>
+                                    <div>
+                                        <img src={image} alt={title} />
+                                    </div>
+                                    <div className='administration-text'>
+                                        <h2>{title}</h2>
+                                        <p>{subtitle}</p>
+                                    </div>
+                                </article>
+                            </a>
+                        )
+                    })
+                }
+            </div>
+
         </>
     )
 }
