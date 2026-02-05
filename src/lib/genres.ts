144bee586ec21921d7bb2572ec6a34ecdacdfc2f
Laconiq <80621625+Laconiq@users.noreply.github.com>
2026-02-05 16:37:59 +0100

    feat: add FPS genre

--- src/lib/genres.ts
+++ src/lib/genres.ts
@@ -1,7 +1,7 @@
 export const ALL_GENRES = [
   'Action', 'Aventure', 'Narratif', 'Plateforme', 'Puzzle',
   'Tactique', 'Rythme', 'Point & Click', 'Deckbuilding',
-  'Rogue Like', 'Metroidvania', 'Horreur', 'VR', '2D', '3D', 'Isométrique'
+  'Rogue Like', 'Metroidvania', 'Horreur', 'VR', '2D', '3D', 'Isométrique', 'FPS'
 ] as const;
 
 export type GameGenre = typeof ALL_GENRES[number];
